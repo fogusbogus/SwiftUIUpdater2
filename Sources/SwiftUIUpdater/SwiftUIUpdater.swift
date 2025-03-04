bb5894d8493e069074583e7ec38751cca3539335
Matt Hogg <matt.hogg74@hotmail.com>
2025-03-04 10:22:19 +0000

    Initial

--- Sources/SwiftUIUpdater/SwiftUIUpdater.swift
+++ Sources/SwiftUIUpdater/SwiftUIUpdater.swift
@@ -1,2 +1,10 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+import SwiftUI
+
+public class SwiftUIUpdater: ObservableObject {
+	@Published public var toggle: Bool = false
+	public func update() {
+		toggle = !toggle
+	}
+}
